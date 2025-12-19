f1804fec9f8301ea71334658d71fb511c4f2b746
Eliot Moss <moss@cs.umass.edu>
2025-12-18 20:21:22 -0500

    Add Tutorial documentation on how to do external linking; refine two tests as a result of that.

--- test/link/export10_.c
+++ test/link/export10_.c
@@ -1,12 +1,13 @@
 # include <stdio.h>
 # include <stdlib.h>
-extern void *getIncr(void *);
+typedef int (*FP)(void *, int);
+
+extern FP getIncr(void *);
 
 int main(int argc, char *argv[]) {
   int i = atoi(argv[1]);
-  void *p = getIncr(NULL);
-  int (*incr)(void *,int) = p;
-  int j = incr(NULL, i - 1);
+  FP incr = getIncr(NULL);
+  int j = (*incr)(NULL, i - 1);
   printf("%d\n", j == 1 ? 89 : 75);
   return 0;
 }
